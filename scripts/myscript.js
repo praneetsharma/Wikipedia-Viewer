c86806d1635621250e292efbefe2756c120983e6
praneetsharma <praneet.223@gmail.com>
2017-02-07 00:33:53 +0530

    Some cosmetic changes. No enhancement in functionality :-(

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
 
   var NUM_RESULTS = 10;
   var RANDOM_WIKI_URL = "https://en.wikipedia.org/wiki/Special:Random";
+  //var WIKI_URL =
 
   function resultTemplate (url, heading, content, id) {
     var res = "<div class='res-box container-fluid' id='res" + id + "'>" +
-                "<a href='" + url + "'>" +
+                "<a href='" + url + "' target='_blank'>" +
                   "<h3>" + heading + "</h3>" +
                   "<h4>" + content + "</h4>" +
                 "</a>" +
@@ -20,11 +21,40 @@ $(document).ready(function() {
     $(".search-results").append(res);
   }
 
+  function clearSearchResults () {
+    $(".search-results").html("");
+  }
+
+  function clearSearchQuery () {
+    $(".form-control").val("");
+  }
+
+  function makeRequestAndPopulateSearchresults(_url, searchQuery) {
+    $.ajax({
+
+    });
+  }
 
   $(".button-random").click(function() {
     window.open(RANDOM_WIKI_URL, "_blank");
   });
 
+  $(".clear-search").click(function () {
+    clearSearchQuery();
+    clearSearchResults();
+  });
+
+  $(".search-icon").click(function() {
+    var searchQuery = $(".form-control").val();
+
+    // Do anything/something only if search query is non-empty
+    if (searchQuery != null && searchQuery !== "") {
+      // clear the existing search results before populating the new ones.
+      clearSearchResults();
+
+      makeRequestAndPopulateSearchresults(url, searchQuery);
+    }
+  });
 
   populateResults();
 
